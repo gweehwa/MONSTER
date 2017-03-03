fb7879cd19d08fa74df6f8c2aa4987cd758c6470
871158192 <871158192@qq.com>
2017-03-03 11:12:02 -0500

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -46,7 +46,8 @@ CTss(int n, double *y[], double *value, double *con_mean, double *tr_mean,
     double tr_var, con_var;
     double con_sqr_sum = 0., tr_sqr_sum = 0.;
     double xz_sum = 0., xy_sum = 0., x_sum = 0., y_sum = 0., z_sum = 0.;
-    double xx_sum = 0., alpha_1 = 0., alpha_0 = 0., beta_1 = 0., beta_0 = 0.;
+    double yz_sum = 0., xx_sum = 0., yy_sum = 0., zz_sum = 0.;
+    double alpha_1 = 0., alpha_0 = 0., beta_1 = 0., beta_0 = 0.;
     double numerator, denominator;
     for (i = 0; i < n; i++) {
         temp1 += *y[i] * wt[i] * treatment[i];
