64dd17f58fe72e24fb8cae2965ecfdd1ea879550
871158192 <871158192@qq.com>
2017-03-03 11:20:08 -0500

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -63,7 +63,7 @@ CTss(int n, double *y[], double *value, double *con_mean, double *tr_mean,
         z_sum += *y[i];
         yz_sum += *y[i] * treatment[i];
         xx_sum += IV[i] * IV[i];
-        yy_sum = y_sum;
+        yy_sum = treatment[i] * treatment[i];
         zz_sum += *y[i] * *y[i];
     }
 
@@ -83,7 +83,7 @@ CTss(int n, double *y[], double *value, double *con_mean, double *tr_mean,
     //(1 - alpha) * (1 + train_to_est_ratio) * twt * (tr_var /ttreat  + con_var / (twt - ttreat));
     numerator = zz_sum + n * alpha_0 * alpha_0 + alpha_1 * alpha_1 * yy_sum - 2 * alpha_0 * z_sum - 2 * alpha_1 * yz_sum + 2 * alpha_0 * alpha_1 * y_sum;
     denominator = n * beta_0 * beta_0 + beta_1 * beta_1 * xx_sum + y_sum * y_sum / n + 2 * beta_0 * beta_1 * x_sum - 2 * beta_0 * y_sum - 2 * beta_1 * x_sum * y_sum / n;
-    *value = numerator / denominator;
+    *value = numerator/ denominator;
     *risk = 4 * twt * max_y * max_y - alpha * twt * effect * effect + (1 - alpha) * (1 + train_to_est_ratio) * twt * (numerator / denominator);
 }
 
