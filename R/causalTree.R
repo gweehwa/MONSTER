da9269f4bbd34cbb790eda5a2defeaecb62940cd
871158192 <871158192@qq.com>
2017-03-05 21:50:42 -0500

    Update causalTree.R

--- R/causalTree.R
+++ R/causalTree.R
@@ -3,12 +3,12 @@
 #
 # 
 
-causalTree <- function(formula, data, weights, treatment, subset, 
+causalTree <- function(formula, data, weights, treatment, IV, subset, 
 					   na.action = na.causalTree, 
 					   split.Rule, split.Honest, HonestSampleSize, split.Bucket, bucketNum = 5,
 					   bucketMax = 100, cv.option, cv.Honest, minsize = 2L, 
 					   x = FALSE, y = TRUE, propensity, control, split.alpha = 0.5, cv.alpha = 0.5,cv.gamma=0.5,split.gamma=0.5,
-					   cost, IV, ...){ 
+					   cost, ...){ 
 
 	Call <- match.call()
 
