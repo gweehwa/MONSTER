f3f843c8520c374eb0b4b34891f5bbf2fd7e33f5
gweehwa <gweehwa@gmail.com>
2017-04-30 07:45:26 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -127,7 +127,7 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
     double left_yz_sum = 0., left_xx_sum = 0., left_yy_sum = 0., left_zz_sum = 0.;
     double alpha_1 = 0., alpha_0 = 0., beta_1 = 0., beta_0 = 0.;
     double numerator, denominator;
-//    double *xz_sum, *xy_sum, *x_sum, *y_sum, *z_sum, *yz_sum, *xx_sum, *yy_sum, *zz_sum; //declare double for categorical
+    double xz_sum = 0., xy_sum = 0., x_sum = 0., y_sum = 0., z_sum = 0., yz_sum = 0., xx_sum = 0., yy_sum = 0., zz_sum = 0.; //declare double for categorical
     for (i = 0; i < n; i++) {
         right_wt += wt[i];
         right_tr += wt[i] * treatment[i];
@@ -326,30 +326,30 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
             wtsqrsums[j] += (*y[i]) * (*y[i]) * wt[i];
             trsqrsums[j] +=  (*y[i]) * (*y[i]) * wt[i] * treatment[i];
                 
-            //xz_sum[j] += *y[i] * IV[i];
-            //xy_sum[j] += treatment[i] * IV[i];
-            //x_sum[j] += IV[i];
-            //y_sum[j] += treatment[i];
-            //z_sum[j] += *y[i];
-            //yz_sum[j] += *y[i] * treatment[i];
-            //xx_sum[j] += IV[i] * IV[i];
-            //yy_sum[j] += treatment[i] * treatment[i];
-            //zz_sum[j] += *y[i] * *y[i];
+            xz_sum += *y[i] * IV[i];
+            xy_sum += treatment[i] * IV[i];
+            x_sum += IV[i];
+            y_sum += treatment[i];
+            z_sum += *y[i];
+            yz_sum += *y[i] * treatment[i];
+            xx_sum += IV[i] * IV[i];
+            yy_sum += treatment[i] * treatment[i];
+            zz_sum += *y[i] * *y[i];
         }
                
         for (i = 0; i < nclass; i++) {
             if (countn[i] > 0) {
                 tsplit[i] = RIGHT;
-        //alpha_1 = (wtsums[j] * xz_sum[j] - x_sum[j] * z_sum[j]) / (wtsums[j] * xy_sum[j] - x_sum[j] * y_sum[j]);
-        //alpha_0 = (z_sum[j] - alpha_1 * y_sum[j]) / wtsums[j];
-        //beta_1 = (wtsums[j] * xy_sum[j] - x_sum[j] * y_sum[j]) / (wtsums[j] * xx_sum[j] - x_sum[j] * x_sum[j]);
-        //beta_0 = (y_sum[j] - beta_1 * x_sum[j]) / wtsums[j];
-        //temp = alpha_1;
-        //numerator = zz_sum[j] + wtsums[j] * alpha_0 * alpha_0 + alpha_1 * alpha_1 * yy_sum[j] - 2 * alpha_0 * z_sum[j] - 2 * alpha_1 * yz_sum[j] + 2 * alpha_0 * alpha_1 * y_sum[j];
-        //denominator = wtsums[j] * beta_0 * beta_0 + beta_1 * beta_1 * xx_sum[j] + y_sum[j] * y_sum[j] / wtsums[j] + 2 * beta_0 * beta_1 * x_sum[j] - 2 * beta_0 * y_sum[j] - 2 * beta_1 * x_sum[j] * y_sum[j] / wtsums[j];
-        //treatment_effect[i] = alpha * temp * temp * wts[j] - (1 - alpha) * (1 + train_to_est_ratio) 
-        //      * wts[j] * (numerator / denominator);
-                treatment_effect[i] = trsums[j] / trs[j] - (wtsums[j] - trsums[j]) / (wts[j] - trs[j]);
+        alpha_1 = (wtsums[j] * xz_sum - x_sum * z_sum) / (wtsums[j] * xy_sum - x_sum * y_sum);
+        alpha_0 = (z_sum - alpha_1 * y_sum) / wtsums[j];
+        beta_1 = (wtsums[j] * xy_sum - x_sum * y_sum) / (wtsums[j] * xx_sum - x_sum * x_sum);
+        beta_0 = (y_sum - beta_1 * x_sum) / wtsums[j];
+        temp = alpha_1;
+        numerator = zz_sum + wtsums[j] * alpha_0 * alpha_0 + alpha_1 * alpha_1 * yy_sum - 2 * alpha_0 * z_sum - 2 * alpha_1 * yz_sum + 2 * alpha_0 * alpha_1 * y_sum;
+        denominator = wtsums[j] * beta_0 * beta_0 + beta_1 * beta_1 * xx_sum + y_sum * y_sum / wtsums[j] + 2 * beta_0 * beta_1 * x_sum - 2 * beta_0 * y_sum - 2 * beta_1 * x_sum * y_sum / wtsums[j];
+        treatment_effect[i] = alpha * temp * temp * wts[j] - (1 - alpha) * (1 + train_to_est_ratio) 
+              * wts[j] * (numerator / denominator);
+        //        treatment_effect[i] = trsums[j] / trs[j] - (wtsums[j] - trsums[j]) / (wts[j] - trs[j]);
             } else
                 tsplit[i] = 0;
         }
