01da59b423efacd96ba4dc4260982f79726a75da
gweehwa <gweehwa@gmail.com>
2017-11-03 11:28:58 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -12,14 +12,7 @@ static int *tsplit;
 static double *wtsqrsums, *trsqrsums;
 static double *xz_sumc, *xy_sumc, *x_sumc, *y_sumc, *z_sumc, *yz_sumc, *xx_sumc, *yy_sumc, *zz_sumc; //declare double for categorical
 
-#include <RInside.h>                    // for the embedded R via RInside
-
-int main(int argc, char *argv[]) {
-    RInside R(argc, argv);              // create an embedded R instance 
-    R["txt"] = "Hello, world!\n";       // assign a char* (string) to 'txt'
-    R.parseEvalQ("cat(txt)");           // eval init string, ignoring returns
-    exit(0);
-}
+#include <Rcpp.h>                    // for the embedded R via RInside
 
 int
 CTinit(int n, double *y[], int maxcat, char **error,
