33979cee27ac045268f5bf18d7e916166ef2ffbd
gweehwa <gweehwa@gmail.com>
2017-12-23 11:34:10 -0500

    Update surrogate.c

--- src/surrogate.c
+++ src/surrogate.c
@@ -19,6 +19,7 @@
 void
 surrogate(pNode me, int n1, int n2)
 {
+    Rprintf("Entered surrogate\n");
     int i, j, k;
     int var;                    /* the primary split variable */
     double split;
