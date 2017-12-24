1ff2c9de6acf26fbbac0d48b438b7e4eac9a1598
gweehwa <gweehwa@gmail.com>
2017-12-23 20:08:28 -0500

    Update causalTree.R

--- R/causalTree.R
+++ R/causalTree.R
@@ -9,7 +9,7 @@ causalTree <- function(formula, data, weights, treatment, IV, subset,
 					   bucketMax = 100, cv.option, cv.Honest, minsize = 2L, 
 					   x = FALSE, y = TRUE, propensity, control, split.alpha = 0.5, cv.alpha = 0.5,cv.gamma=0.5,split.gamma=0.5,
 					   cost, ...){ 
-	#print("Entered causalTree.R\n")
+	print("Entered causalTree.R")
 
 	Call <- match.call()
 
