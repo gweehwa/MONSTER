a8b0f0ef6f98200c761903a0abcc594a07b6a68e
gweehwa <gweehwa@gmail.com>
2017-12-28 10:43:40 -0500

    Update honest_estimate_causalTree.c

--- src/honest_estimate_causalTree.c
+++ src/honest_estimate_causalTree.c
@@ -14,6 +14,7 @@ honest_estimate_causalTree0(const int *dimx, int nnode, int nsplit, const int *d
                             const double *wt2, const double *treatment2, double *IV2, const double *y2,
                             const int *xmiss2, int *where)
 {
+    Rprintf("Entered honest_estimate_causalTree.c\n");
     int i, j;
     int n;
     int ncat;
