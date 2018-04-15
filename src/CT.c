7da7dd0f70493b3ee66482c2c5c8f1a2bade6b48
gweehwa <gweehwa@gmail.com>
2018-04-14 21:43:55 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -227,7 +227,12 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
                 beta_0 = (left_y_sum - beta_1 * left_x_sum) / left_n;
                 left_temp = alpha_1;
                 numerator = (left_zz_sum + left_n * alpha_0 * alpha_0 + alpha_1 * alpha_1 * left_yy_sum - 2 * alpha_0 * left_z_sum - 2 * alpha_1 * left_yz_sum + 2 * alpha_0 * alpha_1 * left_y_sum)/left_n;
-                denominator = left_n * beta_0 * beta_0 + beta_1 * beta_1 * left_xx_sum + left_y_sum * left_y_sum / left_n + 2 * beta_0 * beta_1 * left_x_sum - 2 * beta_0 * left_y_sum - 2 * beta_1 * left_x_sum * left_y_sum / left_n;
+                //denominator = left_n * beta_0 * beta_0 + beta_1 * beta_1 * left_xx_sum + left_y_sum * left_y_sum / left_n + 2 * beta_0 * beta_1 * left_x_sum - 2 * beta_0 * left_y_sum - 2 * beta_1 * left_x_sum * left_y_sum / left_n;
+                denominator = (left_yy_sum / left_n) *
+                              (left_xy_sum / left_n - left_x_sum/left_n * left_y_sum / left_n) * 
+                              (left_xy_sum / left_n - left_x_sum/left_n * left_y_sum / left_n);
+                                
+                
                 left_effect = alpha * left_temp * left_temp * left_wt - (1 - alpha) * (1 + train_to_est_ratio) 
                     * left_wt * (numerator / denominator);
                 Rprintf("Entered CT.c. Left a1, a0, b1, b0 are %.2f, %.2f, %.2f, %.2f.", alpha_1, alpha_0, beta_1, beta_0);
