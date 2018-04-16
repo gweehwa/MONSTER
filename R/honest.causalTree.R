0012c95ebe42a6f51d06b3f3355d68a1e41ab7b2
gweehwa <gweehwa@gmail.com>
2018-04-15 22:32:35 -0400

    Update honest.causalTree.R

--- R/honest.causalTree.R
+++ R/honest.causalTree.R
@@ -42,9 +42,9 @@ honest.causalTree <- function(formula, data, weights, treatment, IV, subset,
 		stop("You should input the treatment status vector for data:
 			 1 represent treated and 0 represent controlled.")   
 	}
-	#if (sum(treatment %in% c(0,1)) != nobs) {
-	#	stop("The treatment status should be 1 or 0 only: 1 represent treated and 0 represent controlled.")
-	#}
+	if (sum(treatment %in% c(0,1)) != nobs) {
+		stop("The treatment status should be 1 or 0 only: 1 represent treated and 0 represent controlled.")
+	}
 	if (sum(treatment) == 0 || sum(treatment) == nobs) {
 		stop("The data only contains treated cases or controlled cases, please check 'treatment' again.") 
 	}
@@ -76,9 +76,9 @@ honest.causalTree <- function(formula, data, weights, treatment, IV, subset,
 	if (missing(est_treatment)) {
 	    stop("Not give the treatment status of honest estimation data set!\n ")
 	}
-	#if (sum(est_treatment %in% c(0,1)) != est_nobs) {
-	#    stop("The treatment status should be 1 or 0 only: 1 represent treated and 0 represent controlled.")
-	#}
+	if (sum(est_treatment %in% c(0,1)) != est_nobs) {
+	    stop("The treatment status should be 1 or 0 only: 1 represent treated and 0 represent controlled.")
+	}
 	if (sum(est_treatment) == 0 || sum(est_treatment) == est_nobs) {
 	    stop("The data only contains treated cases or controlled cases, please check 'est_treatment' again.") 
 	}
