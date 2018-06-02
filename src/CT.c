c99ae9844fcb992989e8991341ee7b2b2fab5bda
gweehwa <gweehwa@gmail.com>
2018-06-01 21:25:24 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -1021,10 +1021,10 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
     bhat_3 = 0;
     var3 = 1000000;
     }                
-Rprintf("Entered CT.c. b0, b1, b2, b3, error2, invOut[15], right_n are %.2f, %.2f, %.2f, %.2f., %.2f, %.2f., %.2f.\n", bhat_0, bhat_1, bhat_2, bhat_3, error2, invOut[15], right_n);
+//Rprintf("Entered CT.c. b0, b1, b2, b3, error2, invOut[15], right_n are %.2f, %.2f, %.2f, %.2f., %.2f, %.2f., %.2f.\n", bhat_0, bhat_1, bhat_2, bhat_3, error2, invOut[15], right_n);
 
-Rprintf("Entered CT.c. right_x1y_sum, right_x2y_sum, right_x3y_sum, right_x4y_sum , right_yy_sum are %.2f, %.2f, %.2f, %.2f., %.2f.\n", right_x1y_sum, right_x2y_sum, right_x3y_sum, right_x4y_sum, right_yy_sum);
-Rprintf("Entered CT.c. right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x2_sum, right_x2x3_sum, right_x2x4_sum,   right_x3x3_sum, right_x3x4_sum right_x4x4_sum are %.2f, %.2f, %.2f, %.2f., %.2f., %.2f., %.2f., %.2f., %.2f.\n",  right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x2_sum, right_x2x3_sum, right_x2x4_sum, right_x3x3_sum, right_x3x4_sum, right_x4x4_sum );
+//Rprintf("Entered CT.c. right_x1y_sum, right_x2y_sum, right_x3y_sum, right_x4y_sum , right_yy_sum are %.2f, %.2f, %.2f, %.2f., %.2f.\n", right_x1y_sum, right_x2y_sum, right_x3y_sum, right_x4y_sum, right_yy_sum);
+//Rprintf("Entered CT.c. right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x2_sum, right_x2x3_sum, right_x2x4_sum,   right_x3x3_sum, right_x3x4_sum right_x4x4_sum are %.2f, %.2f, %.2f, %.2f., %.2f., %.2f., %.2f., %.2f., %.2f.\n",  right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x2_sum, right_x2x3_sum, right_x2x4_sum, right_x3x3_sum, right_x3x4_sum, right_x4x4_sum );
 
                 alpha_1 = (right_n * right_xz_sum - right_x_sum * right_z_sum) / (right_n * right_xy_sum - right_x_sum * right_y_sum);
                 alpha_0 = (right_z_sum - alpha_1 * right_y_sum) / right_n;
@@ -1038,6 +1038,8 @@ Rprintf("Entered CT.c. right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x
                 //    * right_wt * (numerator / denominator);
                 right_effect = alpha * right_temp * right_temp * right_wt - (1 - alpha) * (1 + train_to_est_ratio) 
                     * right_wt * (var3);
+Rprintf("Entered CT.c. Right treatment effect, var3, effect and obs are %.2f, %.2f, %.2f, %.2f.\n", right_temp, var3, right_effect, right_wt);
+
 // PARAMETER!                    
                 if(abs(right_n * right_xy_sum - right_x_sum * right_y_sum) <= 0 * right_n * right_n){
                 //right_temp = right_tr_sum / right_tr - (right_sum - right_tr_sum) / (right_wt - right_tr);
@@ -1057,11 +1059,15 @@ Rprintf("Entered CT.c. right_x1x2_sum, right_x1x3_sum, right_x1x4_sum, right_x2x
                 temp = left_effect + right_effect - node_effect;
                 if (temp > best) {
                     best = temp;
+Rprintf("Improved. Left, right, node effects and best are %.2f, %.2f, %.2f, %.2f.\n", left_effect, right_effect, node_effect, best);
+
                     where = i;               
                     if (left_temp < right_temp){
                         direction = LEFT;
                     }
                     else{
+Rprintf("Not Improved. Left, right and node effects are %.2f, %.2f, %.2f, %.2f.\n", left_effect, right_effect, node_effect, best);
+
                         direction = RIGHT;
                     }
                 }             
