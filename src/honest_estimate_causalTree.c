77c760800596300d5d59786e82afed6ae94bbe54
gweehwa <gweehwa@gmail.com>
2018-06-01 21:01:51 -0400

    Update honest_estimate_causalTree.c

--- src/honest_estimate_causalTree.c
+++ src/honest_estimate_causalTree.c
@@ -443,7 +443,7 @@ next:
 //    for (i = 0; i < n; i++) {
 //        error2 += (*y[i] - bhat_0 - bhat_1 * treatment[i] - bhat_2 * IV[i] - bhat_3 * IV[i] * treatment[i]) * (*y[i] - bhat_0 - bhat_1 * treatment[i] - bhat_2 * IV[i] - bhat_3 * IV[i] * treatment[i]) / (n - 4); 
 //    }
-    error2 = bhat_0*bhat_0 + 2*bhat_0*bhat_1*x1x2_sum[origindx] + 2*bhat_0*bhat_2*x1x3_sum[origindx] + 2*bhat_0*bhat_3*x1x4_sum[origindx] - 2*bhat_0*x1y_sum[origindx] + bhat_1*bhat_1*x2x2_sum[origindx] + 2*bhat_1*bhat_2*x2x3_sum[origindx] + 2*bhat_1*bhat_3*x2x4_sum[origindx] - 2*bhat_1*x2y_sum[origindx] + bhat_2*bhat_2*x3x3_sum[origindx] + 2*bhat_2*bhat_3*x3x4_sum[origindx] - 2*bhat_2*x3y_sum[origindx] + bhat_3*bhat_3*x4x4_sum[origindx] - 2*bhat_3*x4y_sum[origindx] + yy_sum[origindx];
+    error2 = (n1[origindx]*bhat_0*bhat_0 + 2*bhat_0*bhat_1*x1x2_sum[origindx] + 2*bhat_0*bhat_2*x1x3_sum[origindx] + 2*bhat_0*bhat_3*x1x4_sum[origindx] - 2*bhat_0*x1y_sum[origindx] + bhat_1*bhat_1*x2x2_sum[origindx] + 2*bhat_1*bhat_2*x2x3_sum[origindx] + 2*bhat_1*bhat_3*x2x4_sum[origindx] - 2*bhat_1*x2y_sum[origindx] + bhat_2*bhat_2*x3x3_sum[origindx] + 2*bhat_2*bhat_3*x3x4_sum[origindx] - 2*bhat_2*x3y_sum[origindx] + bhat_3*bhat_3*x4x4_sum[origindx] - 2*bhat_3*x4y_sum[origindx] + yy_sum[origindx])/n1[origindx];
            
     var3 = error2 * invOut[15];   
     } else {
