b81d73d55759bc819c85febaddbca2639e8247b6
gweehwa <gweehwa@gmail.com>
2018-09-06 21:53:20 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -87,7 +87,7 @@ CTinit(int n, double *y[], int maxcat, char **error,
         int *size, int who, double *wt, double *treatment, 
         int bucketnum, int bucketMax, double *train_to_est_ratio)
 {
-    RPrintf("Entered CTinit.\n");
+    Rprintf("Entered CTinit.\n");
     if (who == 1 && maxcat > 0) {
         graycode_init0(maxcat);
         countn = (int *) ALLOC(2 * maxcat, sizeof(int));
