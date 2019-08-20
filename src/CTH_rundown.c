73c4a0ea9b2c601e8dc88bf9121e8a9a67bfae5e
gweehwa <gweehwa@gmail.com>
2019-08-20 14:25:29 -0500

    Update CTH_rundown.c

--- src/CTH_rundown.c
+++ src/CTH_rundown.c
@@ -117,6 +117,7 @@ CTH_rundown(pNode tree, int obs, double *cp, double *xpred, double *xtemp, int k
                     tr_sqr_sum += (*ct.ydata[tmp_obs]) * (*ct.ydata[tmp_obs]) * ct.wt[tmp_obs];
                 }
 		n++;
+  	Rprintf("Entered CTH_rundown. ct.IV, ct.ydata, ct.treatment are %f, %f and %f.\n", ct.IV[tmp_obs], *ct.ydata[tmp_obs], ct.treatment[tmp_obs]);      
 		xz_sum += ct.IV[tmp_obs] * *ct.ydata[tmp_obs];
                 xy_sum += ct.IV[tmp_obs] * ct.treatment[tmp_obs];
 		x_sum += ct.IV[tmp_obs];
