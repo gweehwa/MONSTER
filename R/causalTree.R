108b44a4e565b191864319d980d310141b49b272
gweehwa <gweehwa@gmail.com>
2019-08-22 07:11:16 -0500

    Update causalTree.R

--- R/causalTree.R
+++ R/causalTree.R
@@ -259,8 +259,8 @@ causalTree <- function(formula, data, weights, treatment, IV, subset,
 			xval <- 0L
 		} else if (length(xval) == 1L) {
 			## make random groups
-			control_idx <- which(treatment == 0)
-			treat_idx <- which(treatment == 1)
+			control_idx <- which(treatment %% 2 == 0)
+			treat_idx <- which(treatment %% 2 == 1)
 			xgroups <- rep(0, nobs)
 			xgroups[control_idx] <- sample(rep(1L:xval, length = length(control_idx)), length(control_idx), replace = F)
 			xgroups[treat_idx] <- sample(rep(1L:xval, length = length(treat_idx)), length(treat_idx), replace = F)  
