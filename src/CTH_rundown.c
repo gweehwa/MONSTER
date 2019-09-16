244b35a9a55ffe47455a964b3cef0c6cf49cedd0
gweehwa <gweehwa@gmail.com>
2019-09-15 21:57:46 -0500

    Update CTH_rundown.c

--- src/CTH_rundown.c
+++ src/CTH_rundown.c
@@ -134,8 +134,8 @@ CTH_rundown(pNode tree, int obs, double *cp, double *xpred, double *xtemp, int k
 	}    
         double beta_0 = (y_sum - beta_1 * x_sum) / n;
 
-	double numerator = (ct.ydata[obs2][0] - alpha_0 - alpha_1 * ct.treatment[obs2]) * (ct.ydata[obs2][0] - alpha_0 - alpha_1 * ct.treatment[obs2]);
-        //double numerator = zz_sum + n * alpha_0 * alpha_0 + alpha_1 * alpha_1 * yy_sum - 2 * alpha_0 * z_sum - 2 * alpha_1 * yz_sum + 2 * alpha_0 * alpha_1 * y_sum;
+	//double numerator = (ct.ydata[obs2][0] - alpha_0 - alpha_1 * ct.treatment[obs2]) * (ct.ydata[obs2][0] - alpha_0 - alpha_1 * ct.treatment[obs2]);
+        double numerator = (zz_sum + n * alpha_0 * alpha_0 + alpha_1 * alpha_1 * yy_sum - 2 * alpha_0 * z_sum - 2 * alpha_1 * yz_sum + 2 * alpha_0 * alpha_1 * y_sum)/n;
         double denominator = n * beta_0 * beta_0 + beta_1 * beta_1 * xx_sum + y_sum * y_sum / n + 2 * beta_0 * beta_1 * x_sum - 2 * beta_0 * y_sum - 2 * beta_1 * x_sum * y_sum / n;
 	double tmp;
 	if (n > 2 && denominator!=0) {
